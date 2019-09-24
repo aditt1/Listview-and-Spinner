19418aa80d334960d18d88d6cfeb1d7864083587
aditt1 <aditya.halimawan@si.ukdw.ac.id>
2019-09-24 11:14:03 +0700

    Listview and Spinner

---
+++ app/src/main/java/com/example/samplelist/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.samplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
